24ff3d4fc850b8eb2d45093eeb5eb8b7b2d25349
nguyen <nguyen.nc2710@gmail.com>
2024-03-09 17:00:57 +0700

    test: provide some additional unit test cases

--- src/houses/houses.service.spec.ts
+++ src/houses/houses.service.spec.ts
@@ -17,18 +17,58 @@ describe('HousesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return unique house pair counts', () => {
-    const input: HousePairs[] = [
-      ['1', '1 Main St.'],
-      ['1', '1 Main Street'],
-      ['2', '1 Main Street'],
-      ['2', '1 Main Street West'],
-      ['3', '2 Fith Ave'],
-      ['4', '3 Wall Street'],
-    ];
-    const expectedOutput = 3;
+  describe('countUniqueHouseAddress', () => {
+    it('should return unique house pair counts', () => {
+      const input: HousePairs[] = [
+        ['1', '1 Main St.'],
+        ['1', '1 Main Street'],
+        ['2', '1 Main Street'],
+        ['2', '1 Main Street West'],
+        ['3', '2 Fith Ave'],
+        ['4', '3 Wall Street'],
+      ];
+      const expectedOutput = 3;
 
-    const actualOutput = service.countUniqueHouseAddress(input);
-    expect(actualOutput).toEqual(expectedOutput);
+      const actualOutput = service.countUniqueHouseAddress(input);
+      expect(actualOutput).toEqual(expectedOutput);
+    });
+
+    it('should return 1 when all row ids are duplicated', () => {
+      const input: HousePairs[] = [
+        ['1', '1 Main St.'],
+        ['1', '1 Main Street'],
+        ['1', '1 Main Street'],
+        ['1', '1 Main Street West'],
+        ['1', '2 Fith Ave'],
+        ['1', '3 Wall Street'],
+      ];
+      const expectedOutput = 1;
+
+      const actualOutput = service.countUniqueHouseAddress(input);
+      expect(actualOutput).toEqual(expectedOutput);
+    });
+
+    it('should return 1 when all row addresses are duplicated', () => {
+      const input: HousePairs[] = [
+        ['1', '1 Main St.'],
+        ['1', '1 Main St.'],
+        ['1', '1 Main St.'],
+        ['1', '1 Main St.'],
+        ['1', '1 Main St.'],
+        ['1', '1 Main St.'],
+      ];
+      const expectedOutput = 1;
+
+      const actualOutput = service.countUniqueHouseAddress(input);
+      expect(actualOutput).toEqual(expectedOutput);
+    });
+
+    it('should return 0 when the input is empty', () => {
+      const input: HousePairs[] = [];
+      const expectedOutput = 0;
+
+      const actualOutput = service.countUniqueHouseAddress(input);
+      expect(actualOutput).toEqual(expectedOutput);
+    });
   });
 });
