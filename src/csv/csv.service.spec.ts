0d1e18d4fda93285b42281e93d94ab3bacf6ab06
nguyen <nguyen.nc2710@gmail.com>
2024-03-13 18:15:29 +0700

    test: implement more tests to have full coverage for the main services

--- src/csv/csv.service.spec.ts
+++ src/csv/csv.service.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CsvService } from './csv.service';
+import { ReadStream } from 'fs';
+import { Parser, parse } from 'csv-parse';
+import { Readable } from 'stream';
+
+jest.mock('csv-parse');
 
 describe('CsvService', () => {
   let service: CsvService;
@@ -14,5 +19,47 @@ describe('CsvService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('convertCSVBufferToParserStream', () => {
+    it('should return csv parser from file stream', () => {
+      const fileStreamMock = new Readable() as ReadStream;
+
+      (parse as jest.Mock).mockImplementation(() => {
+        return new Parser({ from_line: 2 });
+      });
+
+      jest.spyOn(fileStreamMock, 'pipe').mockImplementation(jest.fn());
+
+      const result = service.convertCSVBufferToParserStream(
+        fileStreamMock,
+        true,
+      );
+
+      expect(result).toBeInstanceOf(Parser);
+    });
+  });
+
+  describe('createFileReadStream', () => {
+    jest.mock('fs', () => ({
+      ...jest.requireActual('fs'),
+      createReadStream: jest.fn().mockImplementation(() => {
+        return new ReadStream();
+      }),
+    }));
+
+    it('should return file read stream', () => {
+      const csvFileMock = {
+        path: 'test',
+        fieldname: 'csvFile',
+        originalname: 'test.csv',
+        encoding: '7bit',
+        mimetype: 'text/csv',
+      } as Express.Multer.File;
+
+      const result = service.createFileReadStream(csvFileMock);
+
+      expect(result).toBeInstanceOf(ReadStream);
+    });
   });
 });
--- src/houses/houses.service.spec.ts
+++ src/houses/houses.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HousePairs, HousesService } from './houses.service';
 import { CsvService } from 'src/csv/csv.service';
+import { parse } from 'csv-parse';
+import { Readable } from 'stream';
+import { ReadStream } from 'fs';
 
 describe('HousesService', () => {
   let service: HousesService;
+  let csvService: CsvService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,6 +15,7 @@ describe('HousesService', () => {
     }).compile();
 
     service = module.get<HousesService>(HousesService);
+    csvService = module.get<CsvService>(CsvService);
   });
 
   it('should be defined', () => {
@@ -69,6 +74,113 @@ describe('HousesService', () => {
 
       const actualOutput = service.countUniqueHouseAddress(input);
       expect(actualOutput).toEqual(expectedOutput);
+    });
+  });
+
+  describe('countUniqueHouseAddressFromFile', () => {
+    it('should return number of unique house addresses', async () => {
+      const input: HousePairs[] = [
+        ['1', '1 Main St.'],
+        ['1', '1 Main Street'],
+        ['2', '1 Main Street'],
+        ['2', '1 Main Street West'],
+        ['3', '2 Fith Ave'],
+        ['4', '3 Wall Street'],
+      ];
+
+      // Convert input array to CSV format
+      const csvString = input.map((row) => row.join(',')).join('\n');
+
+      const csvFileMock = {
+        path: 'test',
+        fieldname: 'csvFile',
+        originalname: 'test.csv',
+        encoding: '7bit',
+        mimetype: 'text/csv',
+        buffer: Buffer.from(csvString),
+      } as Express.Multer.File;
+
+      // Create a mock readable stream from the buffer
+      const readableStream = new Readable() as ReadStream;
+      readableStream.push(csvFileMock.buffer);
+      readableStream.push(null); // Signal end of stream
+
+      jest
+        .spyOn(csvService, 'createFileReadStream')
+        .mockReturnValue(readableStream);
+
+      const parser = parse();
+      jest
+        .spyOn(csvService, 'convertCSVBufferToParserStream')
+        .mockReturnValue(parser);
+
+      readableStream.pipe(parser);
+
+      // Call the method being tested
+      const result = await service.countUniqueHouseAddressFromFile(csvFileMock);
+
+      // Wait for parsing and the 'end' event to complete
+      expect(result).toEqual(3);
+    });
+
+    it('should return number of unique house addresses when the input is larger than the batch size limit', async () => {
+      const input: HousePairs[] = [
+        ['1', '1 Main St.'],
+        ['1', '1 Main Street'],
+        ['2', '1 Main Street'],
+        ['2', '1 Main Street West'],
+        ['3', '2 Fith Ave'],
+        ['4', '3 Wall Street'],
+      ];
+
+      let counter = 0;
+      while (counter < 50) {
+        input.push(
+          ['1', '1 Main St.'],
+          ['1', '1 Main Street'],
+          ['2', '1 Main Street'],
+          ['2', '1 Main Street West'],
+          ['3', '2 Fith Ave'],
+          ['4', '3 Wall Street'],
+        );
+        ++counter;
+      }
+
+      input.sort((a, b) => Number(a[0]) - Number(b[0]));
+
+      // Convert input array to CSV format
+      const csvString = input.map((row) => row.join(',')).join('\n');
+
+      const csvFileMock = {
+        path: 'test',
+        fieldname: 'csvFile',
+        originalname: 'test.csv',
+        encoding: '7bit',
+        mimetype: 'text/csv',
+        buffer: Buffer.from(csvString),
+      } as Express.Multer.File;
+
+      // Create a mock readable stream from the buffer
+      const readableStream = new Readable() as ReadStream;
+      readableStream.push(csvFileMock.buffer);
+      readableStream.push(null); // Signal end of stream
+
+      jest
+        .spyOn(csvService, 'createFileReadStream')
+        .mockReturnValue(readableStream);
+
+      const parser = parse();
+      jest
+        .spyOn(csvService, 'convertCSVBufferToParserStream')
+        .mockReturnValue(parser);
+
+      readableStream.pipe(parser);
+
+      // Call the method being tested
+      const result = await service.countUniqueHouseAddressFromFile(csvFileMock);
+
+      // Wait for parsing and the 'end' event to complete
+      expect(result).toEqual(3);
     });
   });
 });
